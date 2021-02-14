b8abe57af1a20dfefc369aad0e91bbb389db6dcb
bidhan-1988 <79003551+bidhan-1988@users.noreply.github.com>
2021-02-14 15:53:14 +0530

    Delete dummy directory

--- dummy/dbPool.js
+++
@@ -1,8 +0,0 @@
-const props = require('./properties.js');
-const connection_string = 'postgresql://' + props.database_username + ':' + props.database_password +
-  '@' + props.database_host + ':' + props.connection_port + '/' + props.database_name;
-const pgp = require('pg-promise')();
-var db = {};
-db.conn = pgp(connection_string);
-
-module.exports = db;
--- dummy/schema.js
+++
@@ -1,7 +0,0 @@
-const makeExecutableSchema = require('apollo-server-express').makeExecutableSchema;
-const AppSchema = require('apollo-server-express').AppSchema;
-const Resolvers = require('apollo-server-express').Resolvers;
-exports.makeExecutableSchema = makeExecutableSchema({
-    typeDefs: [AppSchema],
-    resolvers: Resolvers
-});
